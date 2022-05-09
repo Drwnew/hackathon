6de544931a4589e70064afcbd8b0fc29bc23b782
Vrfcbv <105105382+Vrfcbv@users.noreply.github.com>
2022-05-09 20:12:34 +0300

    Class Resource
    Realize Class Resource

---
+++ Resource.cpp
@@ -0,0 +1,9 @@
+#include "Resource.h"
+
+int Resource::getCost() {
+	return cost;
+}
+
+void Resource::setCost(int set_cost) {
+	 cost = set_cost;
+}
---
+++ Resource.h
@@ -0,0 +1,10 @@
+#ifndef RESOUCE_H
+#define RESOUCE_H
+class Resource { //класс Resource
+private:
+	int cost; //стоимость
+public:
+	int getCost(); //получение стоимости
+	void setCost(int set_cost); //изменение стоимости
+};
+#endif RESOUCE_H
